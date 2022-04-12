c178165ec180c300d2890cf6204ff93cd92d6bdb
Jaime Aricapa <ja95aricapa@gmail.com>
2022-04-12 05:08:56 -0500

    task 5 v1

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Across The Planes"""
+
+def add_matrices2D(mat1, mat2):
+    """Adds two matrices element-wise"""
+    result = []
+    row = []
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
+        for i in range(len(mat1)):
+            for j in range(len(mat1[0])):
+                row.append(mat1[i][j] + mat2[i][j])
+            result.append(row)
+            row = []
+        return result
+    else:
+        return None
