a4e50430fd8305622b6152622860fcce82baed5a
Jaime Aricapa <ja95aricapa@gmail.com>
2022-04-11 20:52:39 -0500

    task 2 v2

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    x = len(matrix)
-    y = len(matrix[0])
-    list1 = [x, y]
+    list1 = []
+    
+    try:
+        x = len(matrix)
+        list1.append(x)
+    except:
+        pass
+    try:
+        y = len(matrix[0])
+        list1.append(y)
+    except:
+        pass
     try:
         z = len(matrix[0][0])
         list1.append(z)
     except:
         pass
+
     return list1
