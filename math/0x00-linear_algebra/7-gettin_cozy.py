8fcb88ab900b18d518eab0eb6c3aa756b7b3c6e0
Jaime Aricapa <ja95aricapa@gmail.com>
2022-04-12 20:13:53 -0500

    task 7 v1

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Gettin’ Cozy"""
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Concatenates two matrices along a specific axis"""
+    result = []
+    m1 = [x[:] for x in mat1]
+    m2 = [x[:] for x in mat2]
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        result = m1 + m2
+        return result
+    else:
+        if len(mat1) != len(mat2):
+            return None
+        for i in range(len(m1)):
+            result.append(m1[i] + m2[i])
+        return result
+        
