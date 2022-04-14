e39f2695f11dae47a2c0b112496ef5dac0a1a324
Jaime Aricapa <ja95aricapa@gmail.com>
2022-04-13 19:52:47 -0500

    task 14 v1

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Saddle Up"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication"""
+    result = np.matmul(mat1, mat2)
+    return result
