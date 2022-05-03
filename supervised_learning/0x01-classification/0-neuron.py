7b45184216e702b51713feb7864961fdc8b7b4a1
Jaime <ja95aricapa@gmail.com>
2022-05-03 04:18:59 -0500

    first push

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Neuron"""
+import numpy as np
+
+# task 10
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Class constructor"""
+
+        # check if nx is an valid input features to the neuron
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+            self.n = int(n)
+            self.p = float(p)
+        # set values
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
---
+++ supervised_learning/0x01-classification/README.md
@@ -0,0 +1,3 @@
+# 0x00-binary_classification
+
+At the end of this project, you should be able to build your own binary image classifier from scratch using numpy
