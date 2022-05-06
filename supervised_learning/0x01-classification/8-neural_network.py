998fa23e16a528e16e580d620c1b1260cc250f1f
Jaime <ja95aricapa@gmail.com>
2022-05-06 17:21:12 -0500

    task 8 v3

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -9,7 +9,6 @@ class NeuralNetwork:
 
     def __init__(self, nx, nodes):
         """class constructor"""
-        # check frequent errors
         if type(nx) != int:
             raise TypeError('nx must be an integer')
         if nx < 1:
@@ -20,7 +19,7 @@ class NeuralNetwork:
             raise ValueError('nodes must be a positive integer')
 
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros(nodes).reshape(nodes, 1)
+        self.b1 = 0
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
