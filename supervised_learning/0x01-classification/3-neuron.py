8c7358b6b42f808ec58e1a49f85d299ffbd2b233
Jaime <ja95aricapa@gmail.com>
2022-05-06 15:29:39 -0500

    task 2 v5

--- supervised_learning/0x01-classification/3-neuron.py
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
-"""Neuron"""
+"""Neuron Cost"""
 import numpy as np
 
 
-# utils functions
 def sigmoid(z):
-    """apply sigmoid activation method"""
-    result = 1 / (1 + np.exp(-z))
-    return result
+    """Calculates the sigmoid function"""
+    return 1 / (1 + np.exp(-z))
 
 
-# task 10
 class Neuron:
-    """Defines a single neuron performing binary classification"""
+    """defines a single neuron performing
+    binary classification"""
 
     def __init__(self, nx):
-        """Class constructor"""
-
-        # check if nx is a valid input features to the neuron
+        """class constructor"""
         if type(nx) != int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        # set values
+
         self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
@@ -43,20 +39,8 @@ class Neuron:
         return self.__A
 
     def forward_prop(self, X):
-        """calculates the forward propagation of the neuron"""
-        # Forward Propagation is the way to move from the Input layer (left)
-        # to the Output layer (right) in the neural network
-        #
-        # multiply the matrix input data (left layer) with the weights vector
-        # (each node connection with the right) for the neuron
-        #
-        z = np.matmul(self.__W, X)
-        # z is the activity of the next layer
-        # one row for each example and one column for each hidden unit
-        #
-        # add the bias for the neuron to the activity
-        z += self.__b
-        # apply the activation function
+        """Calculates the forward propagation of the neuron"""
+        z = np.matmul(self.__W, X) + self.__b
         self.__A = sigmoid(z)
         return self.__A
 
