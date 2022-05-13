939e7e0a6288df8ed9cc0b6f06024f3d315747a4
Jaime <ja95aricapa@gmail.com>
2022-05-12 19:23:14 -0500

    task 1 v4

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-"""
-Layers
-"""
+"""File that contains the function create_layer"""
 import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
     """
-    Create Layer
+    Args:
+    prev is the tensor output of the previous layer
+    n is the number of nodes in the layer to create
+    activation is the activation function that the layer should use
     """
-    layer = tf.layers.Dense(name='layer', units=n, activation=activation,
-                            kernel_initializer=tf.contrib.layers.
-                            variance_scaling_initializer(mode="FAN_AVG"))
-    return layer(prev)
+    initialize = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=initialize, name="layer")
+
+    new_layer = layer(prev)
+
+    return new_layer
